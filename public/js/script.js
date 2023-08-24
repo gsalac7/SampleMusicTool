8196f2de744ae287b4f4b1f09a63863031195d63
gsalac7 <gsalac7@gmail.com>
2023-08-23 22:02:12 -0700

    Added Midi Controller

--- public/js/script.js
+++ public/js/script.js
@@ -40,6 +40,7 @@ synthSelect.addEventListener('change', (event) => {
 });
 
 const pianoContainer = document.getElementById('piano');
+/*
 const notes = [
     "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
     "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
@@ -47,11 +48,23 @@ const notes = [
     "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
     "C6"
 ];
+*/
+const notes = [
+    "A0", "A#0", "B0",
+    "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
+    "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+    "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+    "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+    "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
+    "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
+    "C8"
+];
 
-notes.forEach(note => {
+notes.forEach((note, index) => {
     const key = document.createElement('div');
+    key.setAttribute('data-key-index', index);
     
-    // Check if note contains '#' to determine if it's a black key
     if (note.includes('#')) {
         key.classList.add('key', 'black');
     } else {
@@ -59,10 +72,8 @@ notes.forEach(note => {
     }
 
     key.addEventListener('click', () => playNote(note));
-
     pianoContainer.appendChild(key);
 });
-
 
 function playNote(note) {
     console.log("Playing Notes");
@@ -87,7 +98,6 @@ themeSwitch.addEventListener('change', () => {
 });
 
 
-// ... (rest of your JS code) ...
 
 // Initialize MIDI
 if (navigator.requestMIDIAccess) {
@@ -109,16 +119,27 @@ function onMIDIFailure() {
     console.warn("Failed to get MIDI access.");
 }
 
+/*
 function onMIDIMessage(event) {
-    var note = event.data[1];
-    var velocity = event.data[2];
+    console.log("Playing midi key: " + event.data)
+    let note = event.data[1];
+    let velocity = event.data[2];
+    let keyIndex = note - 36; 
 
-    // Assuming C2 is 36 in MIDI numbering
-    var keyIndex = note - 36;
+    var midiNote = event.data[1];
+
+    // Find the note string corresponding to the MIDI note number
+    var noteString = notes.find(note => Tone.Frequency(note).toMidi() === midiNote);
+
+
+    const keyElement = pianoContainer.querySelector(`[data-key-index="${keyIndex}"]`);
+    if (!keyElement) return;  // skip if the key doesn't exist
+    console.log("Gets here")
 
     if (event.data[0] === 144 && velocity > 0) { // note on
-        document.querySelector(`#key-${keyIndex}`).classList.add('active');
+        keyElement.classList.add('active');
     } else if (event.data[0] === 128 || (event.data[0] === 144 && velocity === 0)) { // note off
-        document.querySelector(`#key-${keyIndex}`).classList.remove('active');
+        keyElement.classList.remove('active');
     }
 }
+*/
