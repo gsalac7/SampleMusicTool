3113565107b2c39740139f70c82f03a4c1ad71ff
Gabe Salac <gsalac@Gabes-MacBook-Pro-3.local>
2023-08-23 19:18:43 -0700

    Created Synth and Basic Page

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public')); // Serve static files from the "public" directory.
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
